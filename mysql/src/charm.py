58e4c2c0f1cd1360d8366a084621aa5880395926
guillaume <guillaume.belanger27@gmail.com>
2021-09-15 18:40:41 -0400

    Fixes nrf charm

--- mysql/src/charm.py
+++ mysql/src/charm.py
@@ -13,12 +13,12 @@ develop a new k8s charm using the Operator Framework:
 """
 
 import logging
+import time
+from pathlib import Path
 
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus
-from ops.framework import StoredState
-from ops.pebble import Layer
+from ops.model import ActiveStatus, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
@@ -26,64 +26,41 @@ MYSQL_PORT = 3306
 
 
 class MysqlCharm(CharmBase):
-    """Charm the service."""
-
-    _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.port = MYSQL_PORT
-        self.framework.observe(self.on.mysql_pebble_ready, self._on_config_changed)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        logging.info("hello")
+        self.framework.observe(self.on.mysql_pebble_ready, self._on_mysql_pebble_ready)
 
-    def _on_config_changed(self, event):
-        """(Re)Configure mysql pebble layer specification.
-        A new mysql pebble layer specification is set only if it is
-        different from the current specification.
-        """
-        logger.debug("Running config changed handler")
-        container = self.unit.get_container("mysql")
-
-        # Build layer
-        # layers = mysqlLayers(self.config)
-        mysql_layer = self.mysql_layer_2()
-        plan = container.get_plan()
-        service_changed = plan.services != mysql_layer.services
-        if service_changed:
-            container.add_layer("mysql", mysql_layer, combine=True)
-
-        if service_changed:
-            container.stop("mysql")
-            container.start("mysql")
-            logger.info("Restarted mysql container")
-
-        self.unit.status = ActiveStatus()
-
-        # self._on_update_status(event)
-        logger.debug("Finished config changed handler")
-
-    def mysql_layer_2(self):
-        layer_spec = {
-            "summary": "MySQL layer",
-            "description": "Pebble layer configuration for replicated mysql",
+    def _on_mysql_pebble_ready(self, event):
+        container_name = "mysql"
+        container = self.unit.get_container(container_name)
+        pebble_layer = {
+            "summary": "mysql layer",
+            "description": "pebble config layer for mysql",
             "services": {
                 "mysql": {
                     "override": "replace",
-                    "summary": "mysql daemon",
-                    "command": "/usr/local/bin/docker-entrypoint.sh",
-                    "startup": "enabled",
+                    "summary": "mysql",
+                    "command": "docker-entrypoint.sh mysqld",
                     "environment": {
-                        "MYSQL_USER": "test",
-                        "MYSQL_PASSWORD": "test",
-                        "MYSQL_DATABASE": "oai_db",
+                        "MYSQL_ALLOW_EMPTY_PASSWORD": "true",
                         "MYSQL_ROOT_PASSWORD": "linux",
-                        "MYSQL_ALLOW_EMPTY_PASSWORD": "true"
-                    }
+                        "MYSQL_PASSWORD": "test",
+                        "MYSQL_USER": "test",
+                        "MYSQL_DATABASE": "oai_db",
+                    },
                 }
             },
         }
-        return Layer(layer_spec)
+        container.add_layer(container_name, pebble_layer, combine=True)
+        container.push(
+            "/docker-entrypoint-initdb.d/db.sql", Path("templates/db.sql").read_text()
+        )
+        container.start("mysql")
+        self.unit.status = WaitingStatus("Waiting 30 seconds for the service to start")
+        time.sleep(30)
+        self.unit.status = ActiveStatus()
+
 
 if __name__ == "__main__":
     main(MysqlCharm)
--- nrf/src/charm.py
+++ nrf/src/charm.py
@@ -15,57 +15,29 @@ develop a new k8s charm using the Operator Framework:
 import logging
 
 from ops.charm import CharmBase
-from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, WaitingStatus
+import time
 
 logger = logging.getLogger(__name__)
 
 
 class NrfCharm(CharmBase):
-    """Charm the service."""
-
-    _stored = StoredState()
-
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.nrf_pebble_ready, self._on_nrf_pebble_ready)
 
-    def _on_config_changed(self, event):
-        """Handle the config-changed event"""
-        # Get the nrf container so we can configure/manipulate it
-        container = self.unit.get_container("nrf")
-        # Create a new config layer
-        layer = self._nrf_layer()
-
-        if container.can_connect():
-            # Get the current config
-            services = container.get_plan().to_dict().get("services", {})
-            # Check if there are any changes to services
-            if services != layer["services"]:
-                # Changes were made, add the new layer
-                container.add_layer("nrf", layer, combine=True)
-                logging.info("Added updated layer 'nrf' to Pebble plan")
-                # Restart it and report a new status to Juju
-                container.stop("nrf")
-                container.start("nrf")
-                logging.info("Restarted nrf service")
-            # All is well, set an ActiveStatus
-            self.unit.status = ActiveStatus()
-        else:
-            self.unit.status = WaitingStatus("waiting for Pebble in workload container")
-
-    def _nrf_layer(self):
-        """Returns a Pebble configration layer for Gosherve"""
-        return {
+    def _on_nrf_pebble_ready(self, event):
+        container_name = "nrf"
+        container = self.unit.get_container(container_name)
+        pebble_layer = {
             "summary": "nrf layer",
             "description": "pebble config layer for nrf",
             "services": {
                 "nrf": {
                     "override": "replace",
                     "summary": "nrf",
-                    "command": "/openair-nrf/bin/oai_nrf -c /openair-nrf/etc/nrf.conf -o",
-                    "startup": "enabled",
+                    "command": "/bin/bash /openair-nrf/bin/entrypoint.sh /openair-nrf/bin/oai_nrf -c /openair-nrf/etc/nrf.conf -o",
                     "environment": {
                         "INSTANCE": "0",
                         "PID_DIRECTORY": "/var/run",
@@ -73,10 +45,16 @@ class NrfCharm(CharmBase):
                         "NRF_INTERFACE_PORT_FOR_SBI": "80",
                         "NRF_INTERFACE_HTTP2_PORT_FOR_SBI": "9090",
                         "NRF_API_VERSION": "v1"
-                    }
+                    },
                 }
             },
         }
+        container.add_layer(container_name, pebble_layer, combine=True)
+        container.start("nrf")
+        self.unit.status = WaitingStatus("Waiting 30 seconds for the service to start")
+        time.sleep(30)
+        self.unit.status = ActiveStatus()
+
 
 if __name__ == "__main__":
     main(NrfCharm)
